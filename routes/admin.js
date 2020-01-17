a8407fc8c741970c496c83a39cfa2d2c443be10d
zofis <zofishanmaqbool@gmail.com>
2020-01-17 13:03:39 +0500

    sidebar

--- routes/admin.js
+++ routes/admin.js
@@ -3,12 +3,35 @@ var router = express.Router();
 var firebase = require('firebase');
 
 router.get('/', function(req,res){
-
-    res.json("Hello Admin") 
+  res.render("pages/index") 
 });
 
+router.get('/addFood', function(req,res){
+    res.render("pages/addFood") 
+  });
 
+  router.get('/allFoods', function(req,res){
+    res.render("pages/allFoods") 
+  });
 
+  router.get('/foodDetail', function(req,res){
+    res.render("pages/foodDetail") 
+  });
 
+  router.get('/allOrders', function(req,res){
+    res.render("pages/allOrders") 
+  });
+
+  router.get('/orderDetail', function(req,res){
+    res.render("pages/orderDetail") 
+  });
+
+  router.get('/allAppointments', function(req,res){
+    res.render("pages/allAppointments") 
+  });
+
+  router.get('/appointmentDetails', function(req,res){
+    res.render("pages/appointmentDetails") 
+  });
 
 module.exports = router;
