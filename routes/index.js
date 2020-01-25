00efad96074a299251036bfe7867fd2ace47c66f
DELL <insanegirl288@gmail.com>
2020-01-25 11:55:27 +0500

    details and edit options added

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var firebase = require('firebase');
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASEURL,
+  databaseURL: process.env.DATABASE_URL,
   projectId: process.env.PROJECT_ID,
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
